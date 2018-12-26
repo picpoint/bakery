46dc631590c2cc915a6d1c47ea7e0659b5bc4e2f
rmtar <picpoint@yandex.ru>
2018-12-26 20:56:12 +0300

    add gitignore file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+
 
 gulp.task('test', function () {
 	console.log('hello task');
 });
+
+
+gulp.task('preproc', function () {
+	return gulp.src('./src/less/*.less')
+		.pipe(less())
+		.pipe(gulp.dest('./dist/css/'))
+});
+
