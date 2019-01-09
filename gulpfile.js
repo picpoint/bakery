87bcbbbda68d7e3e019cff1d6610f5e325f5a708
picpoint <picpoint@yandex.ru>
2019-01-09 16:43:49 +0300

    work width gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,44 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var smartgrid = require('smart-grid');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
 
 
-gulp.task('test', function () {
-	console.log('hello task');
-});
+
+var smartGridConf = {
+	outputStyle: 'less',
+	colums: 12,
+	offset: '30px',   // межклоночник
+	mobileFirst: false,
+	container: {
+		maxWidth: '1280px',
+		fields: '30px'   // отступ от края экрана
+	},
+	breakPoints: {
+		xl: {
+			width: '1140px',
+			fields: '30px'
+		},
+		lg: {
+			width: '960px',
+			fields: '30px'
+		},
+		md: {
+			width: '720px',
+			fields: '20px'
+		},
+		sm: {
+			width: '540px',
+			fields: '10px'
+		},
+		xs: {
+			width: '480px',
+			fields: '5px'
+		}
+	}
+};
 
 
 gulp.task('preproc', function () {
@@ -13,3 +47,20 @@ gulp.task('preproc', function () {
 		.pipe(gulp.dest('./dist/css/'))
 });
 
+gulp.task('htmlmin', function () {
+	return gulp.src('./src/*.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('grid', function () {
+	smartgrid('./src/less', smartGridConf);
+});
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+});
