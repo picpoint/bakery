b4437ea43f84312d814bfdef0685839a94fa71ff
rmtar <picpoint@yandex.ru>
2019-05-23 23:26:22 +0300

    work to first task

--- src/js/menuTwisted.js
+++ src/js/menuTwisted.js
@@ -1,19 +1,27 @@
 //1) Меню скручивается в гамбурер и обратно (JS)
-const pastryMenu = document.querySelector('.pastry__menu');
 
 function menuTwisted() {
   const widthWin = document.documentElement.clientWidth;
   const heightWin = document.documentElement.clientHeight;
-  console.log(widthWin);
+	const pastryMenu = document.querySelector('.pastry__menu');
+	const pastryHeaderMeny = document.querySelector('.pastry__headerMenu');
+	const hmbrgr = document.querySelector('.hmbrgr');
+  //console.log(widthWin);
+  //console.log(pastryHeaderMeny);
   //console.log(pastryMenu);
+  console.log(hmbrgr);
 
   if (widthWin <= 480) {
     pastryMenu.style.display = 'none';
+    pastryHeaderMeny.style.height = '150px';
+    hmbrgr.style.display = 'block';
   } else {
     pastryMenu.style.display = 'flex';
+		pastryHeaderMeny.style.height = 'auto';
+		hmbrgr.style.display = 'none';
   }
 }
 
 window.addEventListener('resize', menuTwisted);
 
-menuTwisted();
+//menuTwisted();
