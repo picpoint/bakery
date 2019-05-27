b1952916898a9fb4e5601a1dad6e453b2d2b6127
rmtar <picpoint@yandex.ru>
2019-05-27 21:13:51 +0300

    work to slider

--- src/js/pancakesSlider.js
+++ src/js/pancakesSlider.js
@@ -2,8 +2,10 @@ const backbtn = document.querySelector('.pancakes__backbtn');
 const forvardbtn = document.querySelector('.pancakes__forvardbtn');
 const firstimg = document.querySelector('.pancakes__firstimg');
 const secondimg = document.querySelector('.pancakes__secondimg');
+const thirdimg = document.querySelector('.pancakes__thirdimg');
 
 let count = 0;
+
 let arrslide = [
 	'../pict/slide/bekkon.png',
 	'../pict/slide/berries.png',
@@ -17,39 +19,60 @@ let arrslide = [
 	'../pict/slide/vegetables.png'
 ];
 
+let arrnumb = [
+	'../pict/slide/1.jpg',
+	'../pict/slide/2.jpg',
+	'../pict/slide/3.jpg',
+	'../pict/slide/4.jpg',
+	'../pict/slide/5.jpg',
+	'../pict/slide/6.jpg',
+	'../pict/slide/7.jpg',
+	'../pict/slide/8.jpg',
+	'../pict/slide/9.jpg',
+	'../pict/slide/10.jpg'
+];
+
 function previewPictfirst (arr) {
-	count++;
-	console.log(count);
+	//count++;
 	for (let i = 0; i < arr.length; i++) {
 		if (count >= arr.length) {
 			count = 0;
 		} else {
-			return arr[count];
+			return arr[--count];
 		}
 	}
 }
 
 
 function previewPictsecond (arr) {
-	++count;
-	console.log(count);
-	for (let i = 0; i < arr.length; i++) {
+	for (let i = 1; i < arr.length; i++) {
 		if (count >= arr.length) {
 			count = 0;
 		} else {
-			return arr[count];
+			return arr[++count];
 		}
 	}
 }
 
+function previewPictthird (arr) {
+	for (let i = 2; i < arr.length; i++) {
+		if (count >= arr.length) {
+			count = 0;
+		} else {
+			return arr[++count];
+		}
+	}
+}
 
-
-
+/*
 backbtn.addEventListener('click', function () {
-	//firstimg.lastElementChild.setAttribute('src', previewPictfirst(arrslide));
-	secondimg.lastElementChild.setAttribute('src', previewPictsecond(arrslide));
+
 });
+*/
+
 
 forvardbtn.addEventListener('click', function () {
-	console.log('=>');
+	firstimg.lastElementChild.setAttribute('src', previewPictfirst(arrnumb));
+	secondimg.lastElementChild.setAttribute('src', previewPictsecond(arrnumb));
+	thirdimg.lastElementChild.setAttribute('src', previewPictthird(arrnumb));
 });
