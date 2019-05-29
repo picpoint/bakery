6d07391a2ceaaee0b7c997b9c8d65ec4ce2898a2
picpoint <picpoint@yandex.ru>
2019-05-29 16:44:40 +0300

    redid stars pict on icon font-awesome

--- dist/js/cookieRaiting.js
+++ dist/js/cookieRaiting.js
@@ -7,9 +7,11 @@ function setRaiting (arr) {
 	for (let i = 0; i < arr.length; ++i) {
 		stars[i].addEventListener('mouseover', function (e) {
 			for (let j = 0; j <= i; ++j) {
-				stars[j].setAttribute('src', 'pict/star_solid.png');
+				stars[j].classList.remove('fa-star-o');
+				stars[j].classList.add('fa-star');
 			}
-			stars[i+1].setAttribute('src', 'pict/star_transparent.png');
+			stars[i+1].classList.remove('fa-star');
+      stars[i+1].classList.add('fa-star-o');
 			console.log(i+1);
 		});
 	}
