373d9c9e6df14cac39a88d88c82f1a34875522a2
picpoint <picpoint@yandex.ru>
2019-05-30 16:39:29 +0300

    work to function setCookie

--- src/js/cookieRaiting.js
+++ src/js/cookieRaiting.js
@@ -1,23 +1,34 @@
-const blockstars = document.querySelector('.pancakes__stars');
-const stars = blockstars.children;
+const blockstars = document.querySelector('.pancakes__stars');  // доступ к блоку со звёздочками
+const stars = blockstars.children;                              // массив элементов звёздочек
 
+function setRaiting (arr) {                                     // ф-ия для установления рентинга на звёздочках
+	let mass = [];
+	for (let i = 0; i < arr.length; i++) {                        // пробегаемся по массиву всех элементов
 
-function setRaiting (arr) {
-
-	for (let i = 0; i < arr.length; ++i) {
-		stars[i].addEventListener('mouseover', function (e) {
-			for (let j = 0; j <= i; ++j) {
-				stars[j].classList.remove('fa-star-o');
-				stars[j].classList.add('fa-star');
+		arr[i].addEventListener('mouseover', function (e) {         // на каждом i-м элементе добавляем событие наведение мыши
+			for (let j = 0; j <= i; j++) {                            // пробегаемся по всем элементам на которых сработало событие наведения
+				arr[j].classList.remove('fa-star-o');                   // каждому элементу удаляем старый класс пустой звёздочки
+				arr[j].classList.add('fa-star');                        // устанавливаем новый класс закрашенной звезды
 			}
-			stars[i+1].classList.remove('fa-star');
-      stars[i+1].classList.add('fa-star-o');
-			console.log(i+1);
+			arr[i+1].classList.remove('fa-star');                     // для всех остальных элементов делаем наоборот удаляем класс закрашенной
+      arr[i+1].classList.add('fa-star-o');                      // устанавливаем класс пустой звёздочки
+      mass.push(i + 1);                                         // пушим в массив все значения на которых было событие рейтинга
 		});
 	}
-
-
+	console.log(mass);                                            // выводим в консоль значения
+	setCoockie(mass);                                             // вызываем ф-ию для записи куков и передаём туда массив
 }
 
-setRaiting(stars);
+
+
+function setCoockie(arr) {                                      // ф-ия записи кук
+	let result = [];
+	for (let i = 0; i < arr.length; i++) {                        // перебираем все значения из массива
+		result.push(arr[i]);                                        // пушим все в массив
+	}
+	console.log(result);                                          // выводим массив
+	//document.cookie = `stars=${result}`;
+}
+
+setRaiting(stars);                                              // вызываем ф-ию установления рейтинга на звёздах
 
