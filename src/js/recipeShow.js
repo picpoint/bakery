cbfca12cef9d9f37a3758a36731fc2db7089b5f3
picpoint <picpoint@yandex.ru>
2019-06-04 16:55:35 +0300

    work to popup window

--- src/js/recipeShow.js
+++ src/js/recipeShow.js
@@ -1,6 +1,14 @@
 const btn = document.querySelector('.sandwich__recipebtn').lastElementChild;
+const sandwichfullmenu = document.querySelector('.sandwich__fullmenu');
+const sandwichmenublock = document.querySelector('.sandwich__menublock');
 console.log(btn);
+console.log(sandwichfullmenu);
+console.log(sandwichmenublock);
+
 
 function recipeShow () {
-
+  sandwichfullmenu.classList.add('popup');
+  sandwichmenublock.classList.add('popup2');
 }
+
+btn.addEventListener('click', recipeShow);
