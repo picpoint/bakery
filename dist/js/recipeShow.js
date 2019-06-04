90151eeb0820e8d1406fa8814d0d302be2c1163d
rmtar <picpoint@yandex.ru>
2019-06-04 23:52:03 +0300

    work to adaptive popup block

--- dist/js/recipeShow.js
+++ dist/js/recipeShow.js
@@ -1,14 +1,28 @@
 const btn = document.querySelector('.sandwich__recipebtn').lastElementChild;
 const sandwichfullmenu = document.querySelector('.sandwich__fullmenu');
 const sandwichmenublock = document.querySelector('.sandwich__menublock');
-console.log(btn);
-console.log(sandwichfullmenu);
-console.log(sandwichmenublock);
+const sandwichclosebtn = document.querySelector('.sandwich__closebtn');
+const sandwichpictburger = document.querySelector('.sandwich__pictburger');
+const sandwichrecipeburger = document.querySelector('.sandwich__recipeburger');
 
 
 function recipeShow () {
   sandwichfullmenu.classList.add('popup');
   sandwichmenublock.classList.add('popup2');
+  sandwichpictburger.style.display = 'block';
+  sandwichrecipeburger.style.display = 'flex';
+  sandwichclosebtn.style.display = 'flex';
+}
+
+
+function recipeClose () {
+  sandwichfullmenu.classList.remove('popup');
+  sandwichmenublock.classList.remove('popup2');
+  sandwichpictburger.style.display = 'none';
+  sandwichrecipeburger.style.display = 'none';
+  sandwichclosebtn.style.display = 'none';
 }
 
 btn.addEventListener('click', recipeShow);
+sandwichclosebtn.addEventListener('click', recipeClose);
+//sandwichfullmenu.addEventListener('click', recipeClose);
