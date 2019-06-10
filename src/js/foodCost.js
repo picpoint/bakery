6b9a9d7b32a5f474b827b7a544a82b4a0299a888
picpoint <picpoint@yandex.ru>
2019-06-10 11:56:02 +0300

    work to function countPriceMenu

--- src/js/foodCost.js
+++ src/js/foodCost.js
@@ -1,6 +1,27 @@
 const blockread = document.querySelector('.breakfast__btnread');
 const btnread = blockread.firstElementChild;
+const menuscreen = document.querySelector('.breakfast__shadowscreen');
+const closescreen = document.querySelector('.breakfast__closescreen');
+const allId = document.getElementsByTagName('input');
 
-btnread.addEventListener('click', function () {
-  console.log('CATCH');
+
+blockread.addEventListener('click', function () {
+  menuscreen.style.display = 'flex';
 });
+
+closescreen.addEventListener('click', function () {
+  menuscreen.style.display = 'none';
+});
+
+
+
+function countPriceMenu() {
+  /*
+  for (let i = 0; i < allId.length; i++) {
+    allId[i].setAttribute('checked', true);
+  }
+  */
+
+}
+
+countPriceMenu();
