f446c81dc18709c0ba0bcd2394112faa599e1669
rmtar <picpoint@yandex.ru>
2019-06-24 23:23:51 +0300

    launched server on express

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 
+
 app.use(express.static('dist'));
 
-app.listen(3000, () => {
-	console.log('server is starting!');
+app.listen(3000, function () {
+	console.log('server is starting');
 });
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
 	res.render('index.html');
 });
