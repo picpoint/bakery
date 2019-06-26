74540fac534139c31ccac77b7f96d1fe71af1985
picpoint <picpoint@yandex.ru>
2019-06-26 16:57:12 +0300

    work to DB

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
-
+const mysql = require('mysql');
 
 app.use(express.static('dist'));
+
+let connect = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'root',
+	password: 'root',
+	database: 'bakerydb'
+});
 
 app.listen(3000, function () {
 	console.log('server is starting');
 });
 
 app.get('/', function (req, res) {
+	connect.query(
+			'SELECT * FROM new_table',
+			function (err, result) {
+				if (err) {
+					throw new Error('!!! Error !!!' + err.message);
+				} else {
+					console.log(result);
+				}
+      }
+	);
 	res.render('index.html');
 });
