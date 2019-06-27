4a0068e2e379e79b9ec3ae7cc656c583d96205c2
picpoint <picpoint@yandex.ru>
2019-06-27 10:20:45 +0300

    add commentaries

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const express = require('express');
+const express = require('express');         // пордключаем express
 const app = express();
-const mysql = require('mysql');
+const mysql = require('mysql');             // подключаем mysql
 
-app.use(express.static('dist'));
+app.use(express.static('dist'));            // указываем в какой папке лежит статика
 
-let connect = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'bakerydb'
+let connect = mysql.createConnection({      // объект с данными для подключения
+	host: 'localhost',                        // хост
+	user: 'root',                             // юзер
+	password: 'root',                         // пасс
+	database: 'bakerydb'                      // имя БД
 });
 
-app.listen(3000, function () {
+app.listen(3000, function () {              // вешаем прослушку на 3000 порт
 	console.log('server is starting');
 });
 
-app.get('/', function (req, res) {
-	connect.query(
-			'SELECT * FROM bakerydb.new_table',
-			function (err, result) {
-				if (err) {
+app.get('/', function (req, res) {          // при обращении к корню
+	connect.query(                            // делаем запрос
+			'SELECT * FROM new_table',            // выбрать всё из таблицы
+			function (err, result) {              // ф-ия обработчик
+				if (err) {                          // если ошибка, выбрасываем исключение
 					throw new Error('!!! Error !!!');
-				} else {
-					console.log(result);
-          res.render('index.html');
+				} else {                            // иначе
+					console.log(result);              // в консоль результат
+          res.render('index.html');         // рендерим главную страницу
 				}
       }
 	);
