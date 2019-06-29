869b93436611ad3949bc3b1ef8740f33312d67d5
picpoint <picpoint@yandex.ru>
2019-06-29 18:51:59 +0300

    make request to DB

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-const express = require('express');         // пордключаем express
+const express = require('express');                      // пордключаем express
 const app = express();
-const mysql = require('mysql');             // подключаем mysql
+const mysql = require('mysql');                          // подключаем mysql
 
-app.use(express.static('dist'));            // указываем в какой папке лежит статика
+app.use(express.static('dist'));                         // указываем в какой папке лежит статика
 
-let conn = mysql.createConnection({      // объект с данными для подключения
-	host: 'localhost',                        // хост
-	user: 'root',                             // юзер
-	password: 'root',                         // пасс
-	database: 'bakerydb'                      // имя БД
+let conn = mysql.createConnection({                      // объект с данными для подключения
+	host: 'localhost',                                     // хост
+	user: 'root',                                          // юзер
+	password: 'root',                                      // пасс
+	database: 'bakerydb'                                   // имя БД
 });
+
 
 conn.connect(function (err) {
 	if (err) {
 		throw new Error('!!! ERROR !!!' + err.message);
 	} else {
+		conn.query(
+			'SELECT * FROM tablename',
+			function (err, result) {
+				if (err) {
+					throw new Error('!!! ERROR !!! ' + err.message); //
+				} else {
+					console.log(result);
+				}
+      }
+		);
 		console.log('Connection ty MySql Server');
 	}
 });
+
+
 
 app.listen(3000, function () {              // вешаем прослушку на 3000 порт
 	console.log('server is starting');
 });
 
+
 app.get('/', function (req, res) {          // при обращении к корню
-	conn.query(                            // делаем запрос
-			'SELECT * FROM new_table',            // выбрать всё из таблицы
-			function (err, result) {              // ф-ия обработчик
-				if (err) {                          // если ошибка, выбрасываем исключение
-					throw new Error('!!! Error !!!');
-				} else {                            // иначе
-					console.log(result);              // в консоль результат
-          res.render('index.html');         // рендерим главную страницу
-				}
-      }
-	);
+	res.render('index.html');
 });
