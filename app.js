31937b382490915052644647626ce6782f55f2ad
picpoint <picpoint@yandex.ru>
2019-07-03 23:31:31 +0300

    work with check fields

--- app.js
+++ app.js
@@ -12,34 +12,33 @@ let conn = mysql.createConnection({                      // объект с да
 });
 
 
-conn.connect(function (err) {
-	if (err) {
-		throw new Error('!!! ERROR !!!' + err.message);
-	} else {
-		conn.query(
-			'SELECT * FROM tablename',
-			function (err, result) {
-				if (err) {
-					throw new Error('!!! ERROR !!! ' + err.message); //
-				} else {
-					console.log(result);
-					for (let i = 0; i < result.length; i++) {
-						console.log(result[i].name);
+conn.connect(function (err) {                            // ф-ия коннекта
+	if (err) {                                             // если ошибка
+		throw new Error('!!! ERROR !!!' + err.message);      // выбрасываем исключение
+	} else {                                               // иначе
+		conn.query(                                          // делаем запрос
+			'SELECT * FROM tablename',                         // выбрать всё из таблицы tablename
+			function (err, result) {                           // вторым аргументом идт ф-ия
+				if (err) {                                       // если ошибка
+					throw new Error('!!! ERROR !!! ' + err.message); // выбрасываем исключение
+				} else {                                         // иначе
+					for (let i = 0; i < result.length; i++) {      // пробегаемся по результату
+						console.log(result[i].name);                 // на каждом шаге выводим имя (поле в таблице)
 					}
 				}
       }
 		);
-		console.log('Connection ty MySql Server');
+		console.log('Connection ty MySql Server');           // если коннект произошёл к бд, выводим сообщение
 	}
 });
 
 
 
-app.listen(3000, function () {              // вешаем прослушку на 3000 порт
+app.listen(3000, function () {                           // вешаем прослушку на 3000 порт
 	console.log('server is starting');
 });
 
 
-app.get('/', function (req, res) {          // при обращении к корню
+app.get('/', function (req, res) {                       // при обращении к корню
 	res.render('index.html');
 });
--- src/js/registration.js
+++ src/js/registration.js
@@ -1,3 +1,19 @@
 const btnreg = document.querySelector('.regform__btnsend').firstElementChild;
+const login = document.querySelector('#fieldlogin');
+const passw = document.querySelector('#fieldpassw');
+const email= document.querySelector('#fieldemail');
 
-console.log(btnreg);
+
+
+function checkField() {
+  var reglogpass = /^[0-9]$/;
+  var regmail = /^([0-9a-z_\.-]+)@([0-9a-z_\.-]+)\.([a-z]{2,7})$/;
+
+
+  let log = reglogpass.test(login.value);
+  console.log(log);
+}
+
+
+
+btnreg.addEventListener('click', checkField);
